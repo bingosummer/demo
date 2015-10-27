3231e63d61fa303b3c60fd04fc76eae428b48622
Bin Xia <binxi@microsoft.com>
2015-10-27 13:39:07 +0000

    Force to install apache2

--- create_2048_game.sh
+++ create_2048_game.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install apache2 unzip
+sudo apt-get -y install apache2 unzip
 wget https://codeload.github.com/gabrielecirulli/2048/zip/master
 sudo mv master /var/www/html/
 cd /var/www/html/
